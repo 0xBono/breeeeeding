7fcd7605a691edcc40187438cb4a1bd5c2f183e8
0xbono <iam@sonjim.in>
2021-08-10 22:50:23 +0900

    feat(components/card): 이미지 카드 컴포넌트 추가

---
+++ components/Card/ImageCard/ImageCard.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames/bind';
+import styles from './ImageCard.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  texthide?: boolean;
+  small?: boolean;
+}
+
+export const ImageCard: React.FC<Props> = ({ texthide, small }) => {
+  return (
+    <div className={cx('imagecard-wrap')}>
+      <img className={cx('imagecard-img', { small })} src="/assets/temp_img.svg" alt="temp_image" />
+      {!texthide && <p className={cx('imagecard-title')}>입양준비와 초기🐶</p>}
+    </div>
+  );
+};
