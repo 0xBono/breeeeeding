97e2728b87ddfe525a3cb59a17110f077d47f61c
0xbono <iam@sonjim.in>
2021-08-10 22:57:21 +0900

    feat(containers/details): 임시적으로 콘텐츠 삽입

---
+++ containers/Details/contents/data.ts
@@ -0,0 +1,50 @@
+export const Self = [
+  {
+    id: 1,
+    self_title: '제가 추구하는 ‘비전’입니다',
+    self_content:
+      '긍정강화 교육의 권위자 ‘카렌 프라이어’의 정확한 긍정강화 교육으로 아이들과 즐거운 교육을 진행합니다. 보호자님과 반려견의 더 행복한 반려 라이프를 통해 삶의 질을 높이기 위한 교육을 진행합니다.',
+    self_img: '/assets/temp_img.svg',
+  },
+  {
+    id: 2,
+    self_title: '반려견 교육의 ‘전문가’입니다',
+    self_content:
+      '반려견이 스스로 생각하도록 하여 보호자님과 더 깊은 소통을 하고, 더 높은 지능개발에 도움이 되며 문제행동 교정에 효율적인 도구를 사용하는 ‘클리커 트레이닝’이 전문 분야 입니다.',
+    self_img: '/assets/temp_img.svg',
+  },
+  {
+    id: 3,
+    self_title: '교육은 ‘체계적으로’ 진행됩니다',
+    self_content:
+      '행동학 기반의 설문지를 통해 아이를 정밀하게 진단하고, 보호자님과의 상담, 환경등을 파악해서 맞춤형 교육을 실시 합니다. 반려견 교육에 꼭 필요한 이론들을 직접쓴 전자책을통해 교육하며 반려견의 실전 교육도 함께 진행합니다.  교육이 끝난 이후에도 전자책을 보내드리고, 꾸준한 피드백을 통해 지속가능한 교육을 지향합니다.',
+    self_img: '/assets/temp_img.svg',
+  },
+];
+
+export const Prepare = [
+  {
+    id: 1,
+    prepare_img: '/assets/temp_img.svg',
+    prepare_title: '반려견 지도사 자격증',
+  },
+  {
+    id: 2,
+    prepare_img: '/assets/temp_img.svg',
+    prepare_title: '반려동물행동교정사 1급',
+  },
+  {
+    id: 3,
+    prepare_img: '/assets/temp_img.svg',
+    prepare_title: 'Karen pryor ACADEMY Certification',
+  },
+  {
+    id: 4,
+    prepare_img: '/assets/temp_img.svg',
+    prepare_title: 'Karen pryor ACADEMY Certification',
+  },
+];
+
+export const Class = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+
+export const Review = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
