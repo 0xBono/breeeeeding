faf22ccc7065e88a8739ef3867505527f8102dae
0xbono <iam@sonjim.in>
2021-08-15 22:13:16 +0900

    feat(containers/renewal): 전체적인 레이아웃 정의

--- containers/Renewal/Renewal.tsx
+++ containers/Renewal/Renewal.tsx
@@ -1,3 +1,20 @@
+import { GNBLayout } from 'components/Layout';
+import Image from 'next/image';
+import classNames from 'classnames/bind';
+import styles from './Renewal.module.scss';
+
+const cx = classNames.bind(styles);
+
 export const Renewal = () => {
-  return <></>;
+  return (
+    <GNBLayout>
+      <div className={cx('wrap')}>
+        <section className={cx('info-section')} />
+        <section className={cx('details-section')}>
+          <article className={cx('info-inner')} />
+          <article className={cx('review-inner')} />
+        </section>
+      </div>
+    </GNBLayout>
+  );
 };
